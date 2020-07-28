ec97924603826d3be2077210ecc7d33ef8b9ec46
Vishnu P S <vishnu@evolvier.com>
2020-07-29 01:10:48 +0530

    single select added.

--- example/App.js
+++ example/App.js
@@ -3,15 +3,31 @@ import React from 'react';
 import { StyleSheet, View } from 'react-native';
 import ReactNativeSelect from '@evolvier/react-native-select';
 
+const data = [{
+  key: "item1",
+  label: "Item 1"
+}, {
+  key: "item2",
+  label: "Item 2"
+}, {
+  key: "item3",
+  label: "Item 3"
+}, {
+  key: "item4",
+  label: "Item 4"
+}, {
+  key: "item5",
+  label: "Item 5"
+}];
+
 export default function App() {
+
   return (
     <View style={styles.container}>
       <StatusBar />
       <ReactNativeSelect
-        searchInputPlaceholderText={"gggg"}
-        
+        items={data}
       />
-      
     </View>
   );
 }
@@ -23,6 +39,7 @@ const styles = StyleSheet.create({
     // alignItems: 'center',
     // justifyContent: 'center',
     // backgroundColor: "red",
-    marginTop:30
+    padding: 16,
+    marginTop:40
   },
 });
--- src/index.js
+++ src/index.js
@@ -9,58 +9,138 @@ import React, { Component } from "react";
 import {
   Text,
   View,
-  Modal,
-  Alert,
   TouchableHighlight,
   StyleSheet,
+  SafeAreaView,
+  ScrollView
 } from "react-native";
+import Modal from 'react-native-modal';
 import PropTypes from "prop-types";
 
 export class index extends Component {
-  state = {
-    modalVisible: false,
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      modalVisible: false,
+      selectedItem: props.selectedItem,
+      selectedItems: props.selectedItems,
+    };
+  }
+
+  _getColor = () => {
+    const { errorColor, tintColor, baseColor, error } = this.props;
+    const { hasFocus } = this.state;
+
+    if (error) {
+      return errorColor;
+    }
+    if (hasFocus) {
+      return tintColor;
+    }
+    return baseColor;
+  };
+
+  _getLineStyleVariant = () => {
+    const { error } = this.props;
+    const { hasFocus } = this.state;
+
+    return error || hasFocus
+      ? { borderBottomWidth: 2, paddingBottom: 1 }
+      : { borderBottomWidth: 0.5, paddingBottom: 2.5 };
   };
 
   setModalVisible = (visible) => {
     this.setState({ modalVisible: visible });
   };
+
+  _renderSingleSelectItems = (items, onSubmit) => {
+    return (
+      <SafeAreaView>
+        <ScrollView>
+          <View>
+            {items.map(item => (
+              <TouchableHighlight key={item.key} onPress={() => {
+                this.setModalVisible(false);
+                this.setState({
+                  selectedItem: item.key
+                });
+                if (onSubmit) {
+                  onSubmit(item.key);
+                }
+              }}>
+                <Text>{item.label}</Text>
+              </TouchableHighlight>
+            ))}
+          </View>
+        </ScrollView>
+      </SafeAreaView>
+    );
+  };
+
+  _renderItems = (type, items, onSubmit) => {
+    switch (type) {
+      case "select":
+        return this._renderSingleSelectItems(items, onSubmit);
+    }
+  };
+
+  searchItemTextSelect = () => {
+    const { searchInputPlaceholderText, searchInputText, items } = this.props;
+    if (searchInputText) {
+      return searchInputText;
+    } else if (this.state.selectedItem && items.find(item => item.key === this.state.selectedItem)) {
+      return items.find(item => item.key === this.state.selectedItem).label;
+    } else {
+      return searchInputPlaceholderText;
+    }
+  };
+
+  _renderSearchItemText = () => {
+    const { type } = this.props;
+    switch (type) {
+      case "select":
+        return this.searchItemTextSelect();
+    }
+  };
+
+  _renderSearchBox = () => {
+    const { inputContainerStyle } = this.props;
+    const inputContainerBorderStyle = {
+      borderBottomColor: this._getColor(),
+      ...this._getLineStyleVariant()
+    };
+    return (
+      <TouchableHighlight
+        onPress={() => {
+          this.setModalVisible(true);
+        }}
+      >
+        <View style={[
+          styles.inputContainer,
+          inputContainerBorderStyle,
+          inputContainerStyle
+        ]}>
+          <Text>{this._renderSearchItemText()}</Text>
+        </View>
+      </TouchableHighlight>
+    );
+  };
+
   render() {
-    const { props, setModalVisible, state } = this;
-    const { searchInputPlaceholderText } = props;
+    const { props, state } = this;
+    const { type, items, onSubmit } = props;
     return (
       <View>
-        <TouchableHighlight
-          onPress={() => {
-            this.setModalVisible(true);
-          }}
-        >
-          <Text>Click Me</Text>
-        </TouchableHighlight>
-        <Text> react native select </Text>
-
+        {this._renderSearchBox()}
         <Modal
-          animationType="slide"
-          transparent={true}
-		  visible={state.modalVisible}
-		  presentationStyle={'fullScreen'}
-          onRequestClose={() => {
-            Alert.alert("Modal has been closed.");
-          }}
+          style={{ margin: 0, justifyContent: 'flex-end' }}
+          isVisible={state.modalVisible}
         >
-          <View style={styles.centeredView}>
-            <View style={styles.modalView}>
-              <Text style={styles.modalText}>Hello World!</Text>
-
-              <TouchableHighlight
-                style={{ ...styles.openButton, backgroundColor: "#2196F3" }}
-                onPress={() => {
-                  this.setModalVisible(false);
-                }}
-              >
-                <Text style={styles.textStyle}>Hide Modal</Text>
-              </TouchableHighlight>
-            </View>
+          <View style={{ backgroundColor: 'white' }}>
+            {this._renderItems(type, items, onSubmit)}
           </View>
+
         </Modal>
       </View>
     );
@@ -68,51 +148,33 @@ export class index extends Component {
 }
 
 index.propTypes = {
-  searchInputPlaceholderText: PropTypes.String,
+  type: PropTypes.string,
+  items: PropTypes.arrayOf(PropTypes.shape({
+    key: PropTypes.oneOfType([PropTypes.string, PropTypes.number]).isRequired,
+    label: PropTypes.string.isRequired,
+    disabled: PropTypes.bool,
+    children: PropTypes.array,
+  })).isRequired,
+  selectedItem: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+  selectedItems: PropTypes.array,
+  onSubmit: PropTypes.func,
+  searchInputPlaceholderText: PropTypes.string,
+  searchInputText: PropTypes.string,
 };
 
 index.defaultProps = {
-  searchInputPlaceholderText: "Search Item",
+  type: "select",
+  searchInputPlaceholderText: "Select Item",
+  errorColor: 'rgb(213, 0, 0)',
+  tintColor: 'rgb(0, 145, 234)',
+  baseColor: 'rgba(0, 0, 0, .38)',
 };
 
 const styles = StyleSheet.create({
-  centeredView: {
-    flex: 1,
-    // justifyContent: "center",
-    // alignItems: "center",
-    // marginTop: 22,
-  },
-  modalView: {
-	flex: 1,
-    // margin: 20,
-    backgroundColor: "red",
-    // borderRadius: 20,
-    padding: 35,
-    alignItems: "center",
-    shadowColor: "#000",
-    shadowOffset: {
-      width: 0,
-      height: 2,
-    },
-    shadowOpacity: 0.25,
-    shadowRadius: 3.84,
-    elevation: 5,
-  },
-  openButton: {
-    backgroundColor: "#F194FF",
-    borderRadius: 20,
-    padding: 10,
-    elevation: 2,
-  },
-  textStyle: {
-    color: "white",
-    fontWeight: "bold",
-    textAlign: "center",
-  },
-  modalText: {
-    marginBottom: 15,
-    textAlign: "center",
-  },
+  inputContainer: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+  }
 });
 
 export default index;
